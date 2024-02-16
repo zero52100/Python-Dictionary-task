35bebd6b84ab439ae1b8e0e3a74cf87d52d1b1b9
Muhammed Nabeel <57975428+zero52100@users.noreply.github.com>
2024-02-16 17:12:10 +0530

    .

--- Q1.py
+++ Q1.py
@@ -1,1 +1,9 @@
 
+def student(student_name, father_name):
+    student_combined = student_name.copy()
+    student_combined.update(Parents_detail)
+    return student_combined
+Student_detail= {'student_name': "John", 'Class': 6}
+Parents_detail= {'father_name': 'Ali','Mother_name':'Fathima' ,'Contact_no': '911234512345'}
+result = student(Student_detail, Parents_detail)
+print("Student detail:", result)
